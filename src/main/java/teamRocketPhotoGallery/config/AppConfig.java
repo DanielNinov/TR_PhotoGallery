665ca1ea41c7d59e0d0bbaa9620f56130df9d81a
Atanas Minkov <Atanas Minkov>
2016-12-17 13:39:56 +0200

    Update: added FileStorage Functionality, doesn't work yet. Please dont touch it!

---
+++ src/main/java/teamRocketPhotoGallery/config/AppConfig.java
@@ -0,0 +1,14 @@
+package teamRocketPhotoGallery.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import teamRocketPhotoGallery.storage.StorageService;
+
+/**
+ * Created by b4eve on 15-Dec-16.
+ */
+@Configuration
+public class AppConfig {
+
+}
